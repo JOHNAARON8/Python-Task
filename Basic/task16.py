cd7459a4ee73a8b31375da28af72879c7fbe1c75
CadagJohnAron <cadagjohnaron@gmail.com>
2025-05-30 00:12:36 +0800

    Reverse the string

--- Basic/task16.py
+++ Basic/task16.py
@@ -4,3 +4,5 @@ string = str(input("Enter a string: "))
 
 if any(char in checker for char in string):
     print("The string contains special characters.")
+else:
+    print("The string does not contain special characters.")
---
+++ Basic/task17.py
@@ -0,0 +1,5 @@
+string = str(input("Enter a string: "))
+
+rev = string[::-1] 
+
+print(f"The string in reverse order is: {rev}")
