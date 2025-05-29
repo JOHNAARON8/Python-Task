846890c8d766b0ee5bd452c69d2ab3c61293339f
CadagJohnAron <cadagjohnaron@gmail.com>
2025-05-29 23:51:50 +0800

    Convert string to Upper and Lower case

---
+++ Basic/task15.py
@@ -0,0 +1,8 @@
+
+string = str(input("Enter a string: "))
+
+upper = string.upper()
+lower = string.lower()
+
+print(f"The string in uppercase is: {upper}")
+print(f"The string in lowercase is: {lower}")
